72088de738bebb7ae3a8be5ce456043364106723
Jesusalexander <brpoper@gmail.com>
2017-09-18 02:37:10 +0800

    dist subtree

--- src/components/chunk1.js
+++
@@ -1,1 +0,0 @@
-alert('chunk1.js')
--- src/config/dsConfig.js
+++
@@ -1,9 +0,0 @@
-let dsConfig
-let env = process.env['NODE_ENV']
-if (env === 'development') {
-  dsConfig = require('./dsConfig.dev.json')
-} else {
-  dsConfig = require('./dsConfig.prod.json')
-}
-
-export default dsConfig
